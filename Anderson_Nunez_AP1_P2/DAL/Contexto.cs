bd16034e5e0f883156cc96e0f91c577cd5107ae7
Anderson <@1001ander>
2025-07-07 19:45:51 -0400

    Crear la carpetal DAL y el contexto.

---
+++ Anderson_Nunez_AP1_P2/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Anderson_Nunez_AP1_P2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anderson_Nunez_AP1_P2.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Parcial> Parcial { get; set; }
+
+}
