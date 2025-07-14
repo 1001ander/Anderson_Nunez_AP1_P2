4d6cea4ff59e79c1b4be601fbda0a2b85c570865
Anderson <@1001ander>
2025-07-14 19:27:34 -0400

    modelo entrada detalle.

---
+++ Anderson_Nunez_AP1_P2/Models/EntradasDetalle.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Anderson_Nunez_AP1_P2.Models
+{
+    public class EntradasDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("Entrada")]
+        public int EntradaId { get; set; }  
+
+        [Required]
+        [ForeignKey("Producto")]
+        public int ProductoId { get; set; } 
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "La cantidad debe ser mayor que cero.")]
+        public decimal Cantidad { get; set; } 
+
+        
+        public Entradas? Entrada { get; set; }
+        public Productos? Producto { get; set; }
+    }
+}
+
