020b5584870500e831207ca27c6b40e643d33837
Anderson <@1001ander>
2025-07-14 20:06:36 -0400

    primera migracion.

---
+++ Anderson_Nunez_AP1_P2/Migrations/20250715000353_ParcialMej.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Anderson_Nunez_AP1_P2.Migrations
+{
+    /// <inheritdoc />
+    public partial class ParcialMej : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Productos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Descripcion = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Peso = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Existencia = table.Column<int>(type: "int", nullable: false),
+                    EsCompuesto = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Productos", x => x.ProductoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Entradas",
+                columns: table => new
+                {
+                    EntradaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Concepto = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    PesoTotal = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ProductoId = table.Column<int>(type: "int", nullable: false),
+                    CantidadProducida = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entradas", x => x.EntradaId);
+                    table.ForeignKey(
+                        name: "FK_Entradas_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "ProductoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EntradasDetalles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EntradaId = table.Column<int>(type: "int", nullable: false),
+                    ProductoId = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EntradasDetalles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EntradasDetalles_Entradas_EntradaId",
+                        column: x => x.EntradaId,
+                        principalTable: "Entradas",
+                        principalColumn: "EntradaId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EntradasDetalles_Productos_ProductoId",
+                        column: x => x.ProductoId,
+                        principalTable: "Productos",
+                        principalColumn: "ProductoId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Productos",
+                columns: new[] { "ProductoId", "Descripcion", "EsCompuesto", "Existencia", "Peso" },
+                values: new object[,]
+                {
+                    { 1, "Maní", false, 100, 0.25m },
+                    { 2, "Pistachos", false, 100, 0.20m },
+                    { 3, "Almendras", false, 100, 0.15m },
+                    { 4, "Frutos Mixtos 200gr", true, 0, 0.20m },
+                    { 5, "Frutos Mixtos 400gr", true, 0, 0.40m },
+                    { 6, "Frutos Mixtos 600gr", true, 0, 0.60m }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Entradas_ProductoId",
+                table: "Entradas",
+                column: "ProductoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EntradasDetalles_EntradaId",
+                table: "EntradasDetalles",
+                column: "EntradaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EntradasDetalles_ProductoId",
+                table: "EntradasDetalles",
+                column: "ProductoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EntradasDetalles");
+
+            migrationBuilder.DropTable(
+                name: "Entradas");
+
+            migrationBuilder.DropTable(
+                name: "Productos");
+        }
+    }
+}
